a48df2e21bd7e2d74a363aa5fe1ade9bd8050724
VincentAndy <yunishenai@aliyun.com>
2017-08-23 23:24:02 +0800

    My First Commit

---
+++ app/src/main/java/vincent/com/shoppingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package vincent.com.shoppingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
